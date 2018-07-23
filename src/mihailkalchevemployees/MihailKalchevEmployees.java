793524a1360a2e2daa66541112b56b8c870ab0c5
mihail9406 <mihail9406@gmail.com>
2018-07-23 22:49:25 +0300

    corrections

--- src/mihailkalchevemployees/MihailKalchevEmployees.java
+++ src/mihailkalchevemployees/MihailKalchevEmployees.java
@@ -2,8 +2,10 @@ package mihailkalchevemployees;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -11,9 +13,8 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JFileChooser;
+
 /**
  *
  * @author User
@@ -25,7 +26,27 @@ public class MihailKalchevEmployees {
      */
     public static void main(String[] args) throws FileNotFoundException {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                .appendOptional(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+                .optionalStart().appendPattern("dd/MMM/yyyy").optionalEnd()
+                .optionalStart().appendPattern("dd/MM/yyyy").optionalEnd()
+                .optionalStart().appendPattern("dd-MM-yyyy").optionalEnd()
+                .optionalStart().appendPattern("dd.MM.yyyy").optionalEnd()
+                .optionalStart().appendPattern("dd-MMM-yyyy").optionalEnd()
+                .optionalStart().appendPattern("dd.MMM.yyyy").optionalEnd()
+                .optionalStart().appendPattern("yyyy/MM/dd").optionalEnd()
+                .optionalStart().appendPattern("yyyyMMdd").optionalEnd()
+                .optionalStart().appendPattern("yyyy.MM.dd").optionalEnd()
+                .optionalStart().appendPattern("yyyy.MMM.dd").optionalEnd()
+                .optionalStart().appendPattern("yyyy-MMM-dd").optionalEnd()
+                .optionalStart().appendPattern("yyyy/MMM/dd").optionalEnd()
+                .optionalStart().appendPattern("dd/MM/yy").optionalEnd()
+                .optionalStart().appendPattern("dd-MM-yy").optionalEnd()
+                .optionalStart().appendPattern("dd.MM.yy").optionalEnd()
+                .optionalStart().appendPattern("dd/MMM/yy").optionalEnd()
+                .optionalStart().appendPattern("dd-MMM-yy").optionalEnd()
+                .optionalStart().appendPattern("dd.MMM.yy").optionalEnd()
+                .toFormatter();
 
         JFileChooser j = new JFileChooser(); //choose the input file
         j.showSaveDialog(null);
@@ -34,36 +55,30 @@ public class MihailKalchevEmployees {
 
         Map<Integer, ArrayList<Project>> workers = new HashMap(); //HashMap that holds every employee along with his projects
         while (sc.hasNextLine()) { //read from the selected file one line at a time
-            
+
             /*
             Process the data and assign it to required variables for further use
-            */
-            String line = sc.nextLine().trim();
+             */
+            String line = sc.nextLine().replace(" ", "");
             if (line.equals("")) {
                 break;
             }
-            String[] arr = line.split(", ");
+            String[] arr = line.split(",");
             int EmployeeID = Integer.parseInt(arr[0]);
             int projID = Integer.parseInt(arr[1]);
             String startDate = arr[2];
             String endDate = arr[3];
             Date dateFrom = null;
             Date dateTo = null;
-            try {
-                dateFrom = sdf.parse(startDate);
-            } catch (ParseException ex) {
-                Logger.getLogger(MihailKalchevEmployees.class.getName()).log(Level.SEVERE, null, ex);
-            }
+
+            LocalDate dateF = LocalDate.parse(startDate, formatter);
+            dateFrom = Date.from(dateF.atStartOfDay(ZoneId.systemDefault()).toInstant());
 
             if (endDate.equals("NULL")) {
                 dateTo = new Date();
             } else {
-                try {
-                    dateTo = sdf.parse(endDate);
-                } catch (ParseException ex) {
-                    Logger.getLogger(MihailKalchevEmployees.class.getName()).log(Level.SEVERE, null, ex);
-                }
-
+                LocalDate dateT = LocalDate.parse(endDate, formatter);
+                dateTo = Date.from(dateT.atStartOfDay(ZoneId.systemDefault()).toInstant());
             }
 
             Project currentProj = new Project(projID, dateFrom, dateTo);
@@ -78,17 +93,17 @@ public class MihailKalchevEmployees {
 
         }
 
-        System.out.println(findLongestWorkingPair(workers)); 
+        System.out.println(findLongestWorkingPair(workers));
     }
-    
+
     /*
      Method that takes the Map of workers and iterates over it in order to find 
      the two workers who have worked on the same project for the longest overlaping time period
     @return String containing a message - both workers IDs and days they've worked together
-    */
+     */
     public static String findLongestWorkingPair(Map<Integer, ArrayList<Project>> workers) {
         String output = "";
-        int overlapingDays = -1;
+
         int biggestOverlap = 0;
         int firstWorkerID = 0;
         int secondWorkerID = 0;
@@ -100,6 +115,8 @@ public class MihailKalchevEmployees {
             Iterator<Map.Entry<Integer, ArrayList<Project>>> itr2 = workers.entrySet().iterator();
 
             while (itr2.hasNext()) {
+                int overlapingDays = 0;
+
                 Map.Entry<Integer, ArrayList<Project>> nextEntry = itr2.next();
                 ArrayList<Project> nextVal = nextEntry.getValue();
                 for (int i = 0; i < nextVal.size(); i++) {
@@ -115,7 +132,7 @@ public class MihailKalchevEmployees {
                             Date currentProjDateTo = currentVal.get(j).getDateTo();
                             Date nextProjDateFrom = nextVal.get(i).getDateFrom();
                             Date nextProjDateTo = nextVal.get(i).getDateTo();
-                            overlapingDays = numOfDaysOverlaping(currentProjDateFrom, currentProjDateTo,
+                            overlapingDays += numOfDaysOverlaping(currentProjDateFrom, currentProjDateTo,
                                     nextProjDateFrom, nextProjDateTo);
                             if (overlapingDays > biggestOverlap) {
                                 biggestOverlap = overlapingDays;
@@ -127,20 +144,19 @@ public class MihailKalchevEmployees {
                 }
             }
         }
-        if(biggestOverlap == 0){
+        if (biggestOverlap == 0) {
             output = "No such workers";
-        }
-        else{
+        } else {
             output = "Employees: " + firstWorkerID + " and " + secondWorkerID + " have worked together the longest";
             output += " for " + biggestOverlap + " days";
         }
         return output;
     }
-    
+
     /*
     A method that returns the number of overlaping days based on given start 
     and end dates for both projects
-    */
+     */
     public static int numOfDaysOverlaping(Date aStart, Date aEnd, Date bStart, Date bEnd) {
 
         Date latestStart = aStart;
@@ -157,9 +173,8 @@ public class MihailKalchevEmployees {
         if (diffInMillies <= 0) { //if negative => no intersaction between the dates => return 0 days
             return 0;
         } else { //else return the amount of days the projects intersect
-            return (int) TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+            return (int) TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS) + 1;
         }
-
     }
 
 }
